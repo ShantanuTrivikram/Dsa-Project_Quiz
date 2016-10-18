5f7785b157d8b4f3d0504e583acff06ca36fba13
ShantanuTrivikram <trivikram.shantanu@gmail.com>
2016-10-18 21:15:36 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,33 @@
+/********************************************************************************
+ * This Quiz Program developed for recreational purpose
+ * and as a part of project.
+ * Copyright (C) 2016 Shantanu Trivikram trivikram.shantanu@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+*********************************************************************************/
+Shantanu Trivikram
+111503066
+
+
+The present program allows the creation of a Question Bank with single correct multiple choice questions
+and checks the answers and the score
+
+A question bank created is also entered to test the code
+
+Multiple correct questions , Match the Columns, Alpha Numeric answers an be accepted
+
+Running quiz scores can be saved for later use.
+
+
---
+++ quiz_filereading1.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main (int argc, char *argv[]) {
+	char   *buffer, *buffer2,  answer[128];        
+	size_t  i;             
+	size_t  buffer_size;   
+	char   *temp;          
+	char    c, ans, store, Answerkey[128];             
+	FILE   *input_ques, *input_ans;         
+	int j = 1, score, k;
+	if (argc == 1) {
+		fprintf(stderr, "Invalid Argument\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((input_ques = fopen(argv[1], "r")) == NULL) {
+		fprintf(stderr, "File %s does not exist\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	i = 0;
+	buffer_size = BUFSIZ;
+	if ((buffer = malloc(buffer_size)) == NULL) {
+		fclose(input_ques);
+	}
+
+	while ((c = fgetc(input_ques)) != EOF) {
+		if (i == buffer_size) {
+			buffer_size += BUFSIZ;
+			if ((temp = realloc(buffer, buffer_size)) == NULL) {
+				exit(EXIT_FAILURE);
+			}
+	
+			buffer = temp;
+			}
+		buffer[i++] = c;
+		if(c == '*'){
+			printf("%s", buffer);
+//			puts(buffer);
+			scanf("%c",&answer[j]);
+			getchar();
+			j++;
+//			buffer[i] = '\0';
+			i = 0;
+			free(buffer);
+			buffer =(char *)calloc(sizeof(char), buffer_size);
+			strcpy(buffer, "");
+//			puts(buffer2);
+			
+		}
+	//	puts(buffer);	
+	}
+
+	if (i == buffer_size) {
+		buffer_size += 1;
+		if ((temp = realloc(buffer, buffer_size)) == NULL) {
+			exit(EXIT_FAILURE);
+		}
+		buffer = temp;
+	}
+
+	buffer[i] = '\0';
+	while(j != 0) {
+		printf("%c\n",*(answer + j - 1));
+		j--;
+	}
+//	puts(buffer);
+//	scanf("%s", &ans);
+//	if(ans == 'A')
+//		printf("sdf");
+	free(buffer);
+	fclose(input_ques);
+	
+	input_ans = fopen("AnswerKey.txt", "r");
+	while(fscanf(input_ans,"%s", Answerkey) != EOF) {
+		 ;
+	}
+	
+	for(k = 0; k < 128; k++) {
+		if(strcmp(Answerkey[k], answer[k]) == 0) {
+			score++;
+		}
+	}
+	return 0;
+
+}
